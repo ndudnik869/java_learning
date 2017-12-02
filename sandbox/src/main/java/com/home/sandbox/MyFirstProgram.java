25c996f3f78a066a39ece75bebccec98940eeb7a
nadin869 <nady86.86@mail.ru>
2017-12-02 19:43:39 +0300

    переменные и зачения

--- sandbox/src/main/java/com/home/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/com/home/sandbox/MyFirstProgram.java
@@ -2,6 +2,9 @@ package com.home.sandbox;
 
 public class MyFirstProgram {
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
+    System.out.println("Hello," + " World!");
+    int l = 6;
+    int s = l * l;
+    System.out.println("Площадь квадрата со стороной " + l + " = " + s);
   }
 }
