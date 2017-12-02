4b6b6e091eaf15e4add5a9cb377633a562c555e1
nadin869 <nady86.86@mail.ru>
2017-12-02 20:25:20 +0300

    функции

--- sandbox/src/main/java/com/home/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/com/home/sandbox/MyFirstProgram.java
@@ -2,9 +2,26 @@ package com.home.sandbox;
 
 public class MyFirstProgram {
   public static void main(String[] args) {
-    System.out.println("Hello," + " World!");
-    int l = 6;
-    int s = l * l;
-    System.out.println("Площадь квадрата со стороной " + l + " = " + s);
+    hello("Nadezhda");
+    hello("Viktor");
+    double l = 5;
+    double a = 2;
+    double b = 6;
+    System.out.println("Area of square = " + area(l));
+
+    System.out.println("Area of прямоугольника со сторонами " + a + " and " + b + " = " + area(a,b));
+}
+
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
   }
+
+  public static double area(double len){
+    return len * len;
+  }
+
+  public static double area(double a, double b){
+    return a * b;
+  }
+
 }
