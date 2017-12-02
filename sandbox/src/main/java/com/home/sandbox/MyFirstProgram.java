bc0e23304f46f9c7368d270cf27f2194b9f3507d
nadin869 <nady86.86@mail.ru>
2017-12-02 20:51:31 +0300

    Методы функции, ассоциированные с объектами

--- sandbox/src/main/java/com/home/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/com/home/sandbox/MyFirstProgram.java
@@ -8,21 +8,15 @@ public class MyFirstProgram {
     Square s = new Square(5);
     Rectangle r = new Rectangle(2, 6);
 
-    System.out.println("Area of square with side " + s.l +" = " + area(s));
+    System.out.println("Area of square with side " + s.l +" = " + s.area());
 
-    System.out.println("Area of rectangle with sides " + r.a + " and " + r.b + " = " + area(r));
+    System.out.println("Area of rectangle with sides " + r.a + " and " + r.b + " = " + r.area());
   }
 
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
   }
 
-  public static double area(Square s) {
-    return s.l * s.l;
-  }
 
-  public static double area(Rectangle r) {
-    return r.a * r.b;
-  }
 
 }
